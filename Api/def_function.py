dc2bd2207950ebf2337b281e0154c15abc0d22ad
lucasobispo <47404214+lucasobispo@users.noreply.github.com>
2021-01-13 21:54:41 -0300

    Pegando duas categorias e salvando no s3

--- Api/def_function.py
+++ Api/def_function.py
@@ -1,31 +1,33 @@
 import pandas as pd 
-
 from utils import *
 
+category_id = ['MLB1051','MLB1055']
+df_concat = pd.DataFrame(columns=chamando_api(0,'MLB1051')[0].columns)
 
-total_pagina = chamando_api(0)[1]['paging']['total']
+for id in category_id:
+  total_pagina = chamando_api(0,id)[1]['paging']['total']
 
-inicio = 0 
-qtd_por_extracao = 50
-df_concat = pd.DataFrame(columns=chamando_api(0)[0].columns)
+  inicio = 0 
+  qtd_por_extracao = 50
+  
 
-while(inicio < 2500):
-  if inicio >= 0 & inicio<50:
-   df_concat = df_concat.append(chamando_api(0)[0],ignore_index=True)
-   print(inicio)
-   inicio =  inicio + qtd_por_extracao
-  else:
-    if (((total_pagina) - (inicio))  >= 50 ):  
-      df_inicio = chamando_api(inicio)[0]
-      df_concat = df_concat.append(df_inicio,ignore_index=True)
+  while(inicio < 2500):
+    if inicio >= 0 & inicio<50:
+      df_concat = df_concat.append(chamando_api(0,id)[0],ignore_index=True)
       print(inicio)
       inicio =  inicio + qtd_por_extracao
-    else:  
-      df_inicio = chamando_api(inicio)[0]
-      df_concat = df_concat.append(df_inicio,ignore_index=True)
-      print(inicio)
-      inicio =  inicio + ((total_pagina) - (inicio))  
+    else:
+      if (((total_pagina) - (inicio))  >= 50 ):  
+        df_inicio = chamando_api(inicio,id)[0]
+        df_concat = df_concat.append(df_inicio,ignore_index=True)
+        print(inicio)
+        inicio =  inicio + qtd_por_extracao
+      else:  
+        df_inicio = chamando_api(inicio,id)[0]
+        df_concat = df_concat.append(df_inicio,ignore_index=True)
+        print(inicio)
+        inicio =  inicio + ((total_pagina) - (inicio))
 
+df_concat_csv = pd.DataFrame(df_concat).to_csv(index=None, header=True, sep=";")
 
-
-
+upload_to_aws(df_concat_csv, 'dados-mercadolivre/', 'teste.txt')
--- Api/utils.py
+++ Api/utils.py
@@ -1,11 +1,30 @@
 import pandas as pd 
 import urllib.request as urllib2
 import json
+import boto3 as boto3
+from credentials import *
+from botocore.exceptions import NoCredentialsError
 
-def chamando_api(offset):
-  r = urllib2.Request(f'https://api.mercadolibre.com/sites/MLB/search?category=MLB1055&offset={offset}')
+def chamando_api(offset,category_id):
+  r = urllib2.Request(f'https://api.mercadolibre.com/sites/MLB/search?category={category_id}&offset={offset}')
   response = urllib2.urlopen(r)
   elevations = response.read()
   data = json.loads(elevations)
   df = pd.json_normalize(data['results'])
   return df,data 
+
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY,
+                      aws_session_token=AWS_SESSION_TOKEN)
+
+    try:
+        s3.upload_fileobj(local_file, bucket, s3_file, )
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
