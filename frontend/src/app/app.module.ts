fa195d76f820a189fcca6d517504ea85fed666c9
Sabrina <sabrinamonteiro3@outlook.com>
2021-03-21 21:24:19 -0300

    estrutura inicial

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplatesComponent } from './templates/templates.component';
+import { ExternalComponent } from './external/external.component';
+import { HomeComponent } from './external/home/home.component';
+import { AboutComponent } from './external/about/about.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplatesComponent,
+    ExternalComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
