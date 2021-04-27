ff66dc951cddbea97a38d37b5a3befff427029c1
lucasobispo <47404214+lucasobispo@users.noreply.github.com>
2021-04-26 21:20:29 -0300

    Alterando api para apenas 6 variaveis.

--- Api/api_modelo.py
+++ Api/api_modelo.py
@@ -2,15 +2,16 @@ import numpy as np
 from flask import Flask, request, jsonify
 import pickle
 import os
+import json 
+from pandas import json_normalize
 
 app = Flask(__name__)
 
-# modelo = pickle.load(open(r"C:\Users\lucab\Documents\FTT\EC\precificacao_mercadolivre_tcc\Jupyer-Notebooks\precificacao.pk1",'rb'))
+modelo = pickle.load(open(r"C:\Users\lucab\Documents\FTT\EC\precificacao_mercadolivre_tcc\Jupyer-Notebooks\precificacao_4vars.pk1",'rb'))
 
-modelo = pickle.load(open(r"~/precificacao.pk1",'rb'))
+# modelo = pickle.load(open(r"~/precificacao_4vars.pk1",'rb'))
 
-
-modelo = pickle.load(open(r"/Users/Ssilva3/Documents/GitHub/precificacao_mercadolivre_tcc/Jupyer-Notebooks/precificacao.pk1",'rb'))
+# modelo = pickle.load(open(r"/Users/Ssilva3/Documents/GitHub/precificacao_mercadolivre_tcc/Jupyer-Notebooks/precificacao.pk1",'rb'))
 
 @app.route("/")
 def verifica_api_online():
@@ -20,7 +21,11 @@ def verifica_api_online():
 @app.route('/predict', methods=['POST'])
 def predict():
   dados = request.get_json(force=True)
-  predicao = modelo.predict(np.array([list(dados.values())]))
+  json_string = json.dumps(dados)
+  data = json.loads(json_string)
+  dados = json_normalize(data)
+  predicao = modelo.predict(dados)
+  # predicao = modelo.predict(np.array([list(dados.values())]))
   resultado = predicao[0]
 
   resposta = {'PRECO': float(resultado)}
@@ -29,4 +34,4 @@ def predict():
 
 if __name__ == "__main__":
     porta = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=porta)
+    app.run(host='127.0.0.1', port=porta)
